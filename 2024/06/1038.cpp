4a3ce224fac8ab827049b712a0bb180589a29797
Pablo <cardosopab@gmail.com>
2024-06-25 01:35:53 -0500

    Remove TreeNode and its methods

--- 2024/06/1038.cpp
+++ 2024/06/1038.cpp
@@ -12,20 +12,11 @@
 // the node's key. Both the left and right subtrees must also be binary search
 // trees.
 
-#include <cstddef>
+#include "../../tree_utils/include/TreeNode.h"
+#include "../../tree_utils/include/TreeUtils.h"
 #include <iostream>
 #include <optional>
 #include <vector>
-
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
-};
 
 class Solution {
 public:
@@ -51,44 +42,6 @@ public:
     dfs(root->left);
   }
 };
-
-TreeNode *createTreeFromArray(const std::vector<std::optional<int>> &list) {
-  if (list.empty() || !list[0].has_value()) {
-    return nullptr;
-  }
-
-  std::vector<TreeNode *> nodes;
-  for (const auto &val : list) {
-    if (val.has_value()) {
-      nodes.push_back(new TreeNode(val.value()));
-    } else {
-      nodes.push_back(nullptr);
-    }
-  }
-
-  for (size_t i = 0; i < nodes.size(); ++i) {
-    if (nodes[i] != nullptr) {
-      if (2 * i + 1 < nodes.size()) {
-        nodes[i]->left = nodes[2 * i + 1];
-      }
-      if (2 * i + 2 < nodes.size()) {
-        nodes[i]->right = nodes[2 * i + 2];
-      }
-    }
-  }
-  return nodes.empty() ? nullptr : nodes[0];
-}
-
-bool compareTrees(TreeNode *root1, TreeNode *root2) {
-  if (!root1 && !root2)
-    return true;
-  if (!root1 || !root2)
-    return false;
-  if (root1->val != root2->val)
-    return false;
-  return compareTrees(root1->left, root2->left) &&
-         compareTrees(root1->right, root2->right);
-}
 
 void test_solution(Solution *sol) {
   // Example 1:
